6a4d0e669fd387384b29bac17bd79c6ac5a0f361
Buğra Furkan Karadaş <bfK.2002@hotmail.com>
2026-03-07 18:28:07 +0300

    Enemy movment

---
+++ Assets/2-Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager main;
+
+    public Transform startPoint;
+    public Transform[] path;
+    private void Awake()
+    {
+        main = this;
+    }
+}
