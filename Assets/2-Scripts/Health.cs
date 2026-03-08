d9e4d2c3c4013340eb5c9b0f95c80a8be09fbc9a
Furkan Karadas <42517177+PatrnPenguen@users.noreply.github.com>
2026-03-09 01:46:10 +0300

    Update Health.cs

--- Assets/2-Scripts/Health.cs
+++ Assets/2-Scripts/Health.cs
@@ -4,14 +4,16 @@ public class Health : MonoBehaviour
 {
     [Header("Attributes")]
     [SerializeField] private float hitpoints = 2f;
+    private bool isDestroyed = false;
 
     public void TakeDamage(float damage)
     {
         hitpoints -= damage;
 
-        if (hitpoints <= 0)
+        if (hitpoints <= 0 && !isDestroyed)
         {
             EnemySpawner.onEnemyDestroy.Invoke();
+            isDestroyed = true;
             Destroy(gameObject);
         }
     }
